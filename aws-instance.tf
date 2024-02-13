72d8eebd8d74cd40a2e12a2e06520b2c7ef859e5
Surajchavan123 <surajchavan7030@gmail.com>
2024-02-13 15:13:16 +0530

    error solved

--- aws-instance.tf
+++ aws-instance.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-east-2"
 }
-resource "aws_instance" "my_resource" {
+resource "aws_instance" "my_instance" {
     ami = ami-05fb0b8c1424f266b
     instance_type = t2.micro
     key_name = "my-ohio-key"
